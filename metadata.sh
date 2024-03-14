707b1b6ecdd33b9ba5e36e9bf26a7d2992f8c550
Saurabh112211 <raj2saurabh@gmail.com>
2024-03-14 17:15:52 +0530

    metadata

--- metadata.sh
+++ metadata.sh
@@ -6,10 +6,9 @@ if ! command -v aws &> /dev/null; then
     exit 1
 fi
 
-#Retrieve instance metadata and format as JSON 
+#################Retrieve instance metadata and format as JSON##############
 instance_metadata=$(aws ec2 describe-instances --instance-id $(curl -s http://18.233.152.140/latest/meta-data/instance-id) --query 'Reservations[0].Instances[0]' --output json)
-
-#Check if the AWS CLI command was successful 
+ 
 if [ $? -eq 0 ]; then
     # Print the JSON output
     echo "$instance_metadata"
